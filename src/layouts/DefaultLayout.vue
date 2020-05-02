45855a98ad99454d78ec2bc1974e6c9ff36b5547
piotr-s <piotr.slawinski1@gmail.com>
2020-05-02 23:35:14 +0200

    Modify hamburger

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -4,7 +4,7 @@
       <div class="flex items-center justify-between sm:p-0">
         <h2 class="font-sans font-black text-2xl pt-3">{{ $static.metadata.siteName }}</h2>
         <div class="sm:hidden">
-          <button @click="isOpen = !isOpen" type="button" class="p-2 neo-button active:neo-button-depressed block text-black hover:text-gray-600 focus:outline-none">
+          <button @click="isOpen = !isOpen" type="button" :class="isOpen ? 'neo-button-depressed' : 'neo-button'" class="p-2 neo-button active:neo-button-depressed block text-black hover:text-gray-600 focus:outline-none">
             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
               <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
               <path v-else fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
